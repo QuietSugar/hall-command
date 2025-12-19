4d1ef0b25882e6443603281894baccfbc8bc0d63
霍旭 <sugarjellyfish@gmail.com>
2025-12-19 09:49:32 +0800

    增加proxy文件

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -f ".gitkeep" ] && [ "$(cat .gitkeep)" = "HALL-COMMAND" ] || { echo "不是预期目录"; exit 1; }
+[ -f "INFO" ] && [ "$(sed 's/^[[:space:]]*//;s/[[:space:]]*$//' .gitkeep)" = "HALL-COMMAND" ] || { echo "不是预期目录"; exit 1; }
 
 if ! command -v git &> /dev/null; then
     echo "错误: 系统中未安装 git 命令"
@@ -18,14 +18,28 @@ if ! curl -s --connect-timeout 1 https://github.com/QuietSugar/hall-command.git 
     exit 1
 fi
 
-# 初始化 git 仓库并关联远程仓库
+# 检查是否存在代理设置
+if [[ -n "$https_proxy" || -n "$HTTPS_PROXY" ]]; then
+    GIT_PROXY_CONFIG="-c http.proxy=$https_proxy"
+elif [[ -n "$http_proxy" || -n "$HTTP_PROXY" ]]; then
+    GIT_PROXY_CONFIG="-c http.proxy=$http_proxy"
+else
+    GIT_PROXY_CONFIG=""
+fi
+
+# 使用初始化和拉取命令时加入代理配置（如有）
 git init
 git remote add origin https://github.com/QuietSugar/hall-command.git
+
+# 应用重置与清理操作
 git reset --hard
 git clean -fd
-# git -c https.proxy=http://proxy.server:port pull origin master
-git pull origin master
+
+# 根据是否有代理决定如何执行 pull
+if [ -n "$GIT_PROXY_CONFIG" ]; then
+    git $GIT_PROXY_CONFIG pull origin master
+else
+    git pull origin master
+fi
+
 git status
-
-
-
